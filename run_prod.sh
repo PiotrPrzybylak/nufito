ea51d2e01b34a4114d6dcd654025b6829b79f6e0
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-29 15:32:50 +0100

    a script to run docker nufito-prod image

---
+++ run_prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull $1/nufito-prod:latest
+if docker stop nufito-app; then docker rm nufito-app; fi
+echo "running image"
+docker run -d -p 8080:8080 --name nufito-app $1/nufito-prod
+echo "running docker rmi"
+if docker rmi $(docker images --filter "dangling=true" -q --no-trunc); then :; fi
