19611f5c4080d02a2d840c65922fea9676e553a8
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-30 16:40:20 +0100

    --net=host added to see db exposed on host

--- run_prod.sh
+++ run_prod.sh
@@ -3,6 +3,6 @@
 docker pull $1/nufito-prod:latest
 if docker stop nufito-app; then docker rm nufito-app; fi
 echo "running image"
-docker run -d -p 8080:8080 --name nufito-app $1/nufito-prod
+docker run -d -p 8080:8080 --net=host --name nufito-app $1/nufito-prod
 echo "running docker rmi"
 if docker rmi $(docker images --filter "dangling=true" -q --no-trunc); then :; fi
